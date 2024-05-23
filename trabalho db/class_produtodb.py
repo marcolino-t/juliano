17fd3770c5b3cecd2fdaaeefb2905bce80ce4c64
Thiago Marcolino <126633741+MarcolinoT@users.noreply.github.com>
2024-05-23 12:11:48 -0300

    trabalho

--- trabalho db/class_produtodb.py
+++ trabalho db/class_produtodb.py
@@ -2,12 +2,12 @@ import mysql.connector
 
 class BancoDados:
     def __init__(self):
-        self.conexao = mysql.connector.connect(host='127.0.0.1', user='root', passwd='root', database= 'produtos')
+        self.conexao = mysql.connector.connect(host='127.0.0.1', user='root', passwd='root', database= 'produto')
         self.cursor = self.conexao.cursor()
 
         #cria a tabela 'produto' se nao existir
         self.cursor.execute("""
-                            CREATE TABLE IF NOT EXISTS tbproduto(
+                            CREATE TABLE IF NOT EXISTS produto(
                                 id INTEGER PRIMARY KEY AUTO_INCREMENT,
                                 nome VARCHAR(255),
                                 preco FLOAT,
@@ -18,29 +18,29 @@ class BancoDados:
     
     def cadastrar_produto(self, produto):
         self.cursor.execute("""
-            INSERT INTO tbproduto (nome, preco, quantidade)
+            INSERT INTO produto (nome, preco, quantidade)
             VALUES (%s,%s,%s)
         """, (produto.nome, produto.preco, produto.quantidade))
         self.conexao.commit()
 
     def listar_produto(self):
         self.cursor.execute("""
-            SELECT id, nome, preco, quantidade FROM tbproduto            
+            SELECT id, nome, preco, quantidade FROM produto            
         """)
         return self.cursor.fetchall()
     
     def atualizar_produto(self, produto, produto_id):
         self.cursor.execute("""
-            UPDATE tbproduto SET
+            UPDATE produto SET
                 nome = %s,
                 preco = %s,
                 quantidade = %s
             WHERE id = %s
-        """, (produto.nome, produto.email, produto.telefone, produto_id))
+        """, (produto.nome, produto.preco, produto.quantidade, produto_id))
         self.conexao.commit()
 
     def deletar_produto(self, produto_id):
         self.cursor.execute("""
-            DELETE FROM tbproduto WHERE id = %s
+            DELETE FROM produto WHERE id = %s
         """, (produto_id,))
         self.conexao.commit()
